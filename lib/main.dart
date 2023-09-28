f5ecd161235f3ef3b6501842e075e149f1a4e3df
Nikolas Pateras <nikolas.98@live.com>
2023-09-28 11:13:25 +0300

    Added orientation responsiveness and made the widgets more adaptive to iOS.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:expenses_app/widgets/expenses.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
@@ -12,6 +13,13 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
+  WidgetsFlutterBinding. ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight
+  ]);
+
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
